91c4831ef2bf575873fe40dd7a5735a0fe3ad4d4
Patrick Shaw <Patrick.Leong.Shaw@gmail.com>
2017-08-06 00:25:47 +1000

    Corrected dummyState

--- state/dummyState.jsx
+++ state/dummyState.jsx
@@ -4,19 +4,19 @@ const capacity = 100;
 let currentCount = 30;
 const initial = [];
 for(let i = 0; i < 5; i++) {
-  initial.push([
-    i, 
+  initial.push(
     {
+      id: i,
       count: currentCount,
       capacity
     }
-  ]);
+  );
   currentCount += 10;
 }
 const mapState = new MapState(initial);
 setInterval(() => {
-  for(let key of mapState.keys()) {
-    const roomData = mapState.get(key);
+  for(let key of mapState.allRoomData.keys()) {
+    const roomData = mapState.allRoomData.get(key);
     roomData.count = Math.random() * roomData.capacity;
   }
 });
