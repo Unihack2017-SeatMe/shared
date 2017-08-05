04495670ec150c37c7362241ffc01c122a3cc53f
Patrick Shaw <Patrick.Leong.Shaw@gmail.com>
2017-08-06 08:25:08 +1000

    Changed dummy state

--- state/dummyState.tsx
+++ state/dummyState.tsx
@@ -2,22 +2,22 @@ import { MapState } from '../model/MapState';
 let currentId = 0;
 const capacity = 100;
 let currentCount = 30;
-const initial = [];
-for(let i = 0; i < 5; i++) {
-  initial.push([
-    i, 
-    {
+const initial: Array<any> = [];
+for(let i = 0; i < 19; i++) {
+  initial.push(
+    { 
+      id: i,
       count: currentCount,
       capacity
     }
-  ]);
+  );
   currentCount += 10;
 }
 const mapState = new MapState(initial);
 setInterval(() => {
-  for(let key of mapState.keys()) {
-    const roomData = mapState.get(key);
+  for(let key of mapState.allRoomData.keys()) {
+    const roomData = mapState.allRoomData.get(key);
     roomData.count = Math.random() * roomData.capacity;
   }
-});
+}, 2000);
 export { mapState };
